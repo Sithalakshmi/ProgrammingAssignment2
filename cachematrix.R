bf308a6bf1f75bddc615bcb9364e7f7f77847d69
SITHALAKSHMI <asithalakshmi@hotmail.com>
2015-06-20 23:26:30 -0500

    Assg2_CacheMatrix
    Function to create and cache inverse of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This has a pair of functions that creates a special "matrix" object 
+## cache its inverse
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mat <- NULL
+  set <- function(y) {
+    x <<- y
+    mat <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) mat <<- inverse
+  getinv <- function() mat
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix"
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  mat <- x$getinv()
+  if(!is.null(mat)) {
+    message("getting cached data")
+    return(mat)
+  }
+  data <- x$get()
+  mat <- solve(data, ...)
+  x$setinv(mat)
+  mat
 }
